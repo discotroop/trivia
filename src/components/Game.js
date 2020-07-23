ea0cef0ac6dcf3d0346646c26bfb84225ea73f00
John Mark Goodson <john.mark.hat@gmail.com>
2020-07-23 17:37:07 -0400

    shuffleArray is spitting out a this binding err/undefined, may be calling it in the wrong location

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,6 +8,17 @@ import Cards from './Cards.js'
 // strucute it out, who is the final source of truth for this Game ?
 
 class Game extends React.Component {
+
+  constructor(props) {
+        super(props);
+        this.shuffleArray = this.shuffleArray.bind(this);
+        this.state = {
+          questions: [],
+          iterator: 0,
+        }
+      }
+
+
   render() {
     return (
         <div> hello 
@@ -20,16 +31,39 @@ state = {
     questions: []
 };
 
+shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
 componentDidMount() {
     fetch('https://opentdb.com/api.php?amount=10')
         .then(res => res.json())
         .then((data) => {
+          data.results.forEach(function(question) {
+            question.incorrect_answers.push(question.correct_answer);
+           //question.incorrect_answers = shuffleArray(question.incorrect_answers);
+
+          })
             this.setState({ questions: data.results })
             console.log(this.state.questions)
         })
         .catch(console.log)
     }
 }
+//     data.results.forEach(function(question) {
+//       console.log(question)
+//       // consolidate correct answer into incorrect_answers array
+//       question.incorrect_answers.push(question.correct_answer);
+//       // shuffle the order of the incorrect_answers array to randomize order.
+//       question.incorrect_answers = this.shuffleArray(question.incorrect_answers);
+//       // add formatted question to Questions array
+//       Questions.push(question);
+//     })
+
 
 export default Game;
 
@@ -54,13 +88,7 @@ export default Game;
 //     )
 //   }
 
-//   shuffleArray(array) {
-//     for (let i = array.length - 1; i > 0; i--) {
-//         const j = Math.floor(Math.random() * (i + 1));
-//         [array[i], array[j]] = [array[j], array[i]];
-//     }
-//     return array;
-//   }
+
 //   componentDidMount() {
 //     let Questions = [];
 //     fetch('https://opentdb.com/api.php?amount=10')

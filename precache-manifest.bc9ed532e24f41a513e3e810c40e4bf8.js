a98c6d0e12614dddf874fc4a8433b3b9c6bf9719
John Mark Goodson <john.mark.hat@gmail.com>
2020-08-25 14:27:46 -0400

    Updates

---
+++ precache-manifest.bc9ed532e24f41a513e3e810c40e4bf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3222789daeb33c798e363ac9ad77d730",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "53a3a2809ce1da60e10a",
+    "url": "/trivia/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a0edc69f7b249de9d072",
+    "url": "/trivia/static/css/main.e9b82271.chunk.css"
+  },
+  {
+    "revision": "53a3a2809ce1da60e10a",
+    "url": "/trivia/static/js/2.fdef77b4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/trivia/static/js/2.fdef77b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0edc69f7b249de9d072",
+    "url": "/trivia/static/js/main.5ff02f83.chunk.js"
+  },
+  {
+    "revision": "ef294555bde0349958cc",
+    "url": "/trivia/static/js/runtime-main.36b073f9.js"
+  }
+]);
